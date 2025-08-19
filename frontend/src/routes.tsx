9665ada778c60e1b0f61c8a570e04bce710dd200
anudeep22003 <anudeep.yegireddi@gmail.com>
2025-08-19 13:04:26 -0700

    chore: removed installed apps from pages as well as routes
    This helps clean the slate for future development. Otherwise changes
    ripple into that which is out of scope.

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,38 +1,8 @@
 import HumanAiWorkspace from "./pages/HumanAiWorkspace";
-import ConversationalWorkspaceChatPage from "./pages/ConversationalWorkspaceChatPage";
-import ConversationalTriVizPlaygroundPage from "./pages/ConversationalTriVizPlaygroundPage";
-import ThreeJsCdnShowcaseWithFallbackPage from "./pages/ThreeJsCdnShowcaseWithFallbackPage";
-import HelloWorldShadcnGrayscaleLanding from "./pages/HelloWorldShadcnGrayscaleLanding";
-import TwitterDataChatPageWithSocketKnowledgeStream from "./pages/TwitterDataChatPageWithSocketKnowledgeStream";
-import TwitterKnowledgeChatPage from "./pages/TwitterKnowledgeChatPage";
 
 export const routes = [
   {
     path: "/",
     element: <HumanAiWorkspace />,
   },
-  {
-    path: "/conversational-workspace-chat",
-    element: <ConversationalWorkspaceChatPage />,
-  },
-  {
-    path: "/conversational-tri-viz-playground",
-    element: <ConversationalTriVizPlaygroundPage />,
-  },
-  {
-    path: "/threejs-cdn-showcase",
-    element: <ThreeJsCdnShowcaseWithFallbackPage />,
-  },
-  {
-    path: "/hello-world-shadcn-grayscale-landing",
-    element: <HelloWorldShadcnGrayscaleLanding />,
-  },
-  {
-    path: "/twitter-data-chat",
-    element: <TwitterDataChatPageWithSocketKnowledgeStream />,
-  },
-  {
-    path: "/twitter-knowledge-chat",
-    element: <TwitterKnowledgeChatPage />,
-  }
 ];
