bfbe463dae893a1b21c9fab24637cf8b96460d35
anudeep22003 <anudeep.yegireddi@gmail.com>
2025-09-27 11:39:32 -0400

    chore: hardcoded backend url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,5 @@
 export const BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+  import.meta.env.VITE_BACKEND_URL ||
+  "https://github-understand-service-75f675b-e6j25jyuza-uc.a.run.app";
 
 console.log("BACKEND_URL", BACKEND_URL);
